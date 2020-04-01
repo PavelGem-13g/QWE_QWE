744fd20aaeda1a74b3d50419d26ecc6dbb6eb1f6
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-04-01 11:57:24 +0300

    qwe

--- QWE_QWE/Bots/EchoBot.cs
+++ QWE_QWE/Bots/EchoBot.cs
@@ -11,10 +11,32 @@ namespace Microsoft.BotBuilderSamples.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        public string test(string s)
+        {
+            switch (s)
+            {
+                case "a":
+                case "A":
+                case "e":
+                case "E":
+                case "i":
+                case "I":
+                case "o":
+                case "O":
+                case "y":
+                case "Y":
+                case "u":
+                case "U":
+                    return "Гласная";
+
+                default:
+                    return "Согласная или что-то другое, но точно не гласная";
+            }
+        }
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             var responseMessage = turnContext.Activity.Text;
-            if(responseMessage=="1") await turnContext.SendActivityAsync($"Welcome!");
+            await turnContext.SendActivityAsync(test(responseMessage));
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
