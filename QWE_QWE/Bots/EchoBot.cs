bc090b6f4c59c85409428f195a108fc846ab5829
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-04-01 21:10:59 +0300

    qwe

--- QWE_QWE/Bots/EchoBot.cs
+++ QWE_QWE/Bots/EchoBot.cs
@@ -1,42 +1,38 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Schema;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
     public class EchoBot : ActivityHandler
     {
-        public string test(string s)
+        public static int kolvo(ushort N)
         {
-            switch (s)
+            int k = 0;
+            for (ushort i = 1 << 15; i > 0; i = (ushort)(i >> 1))
             {
-                case "a":
-                case "A":
-                case "e":
-                case "E":
-                case "i":
-                case "I":
-                case "o":
-                case "O":
-                case "y":
-                case "Y":
-                case "u":
-                case "U":
-                    return "Vowel";
-
-                default:
-                    return "Consonant or something else, but definitely not a vowel";
+                if ((N & i) == 0) k++;
             }
+            return k;
         }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var responseMessage = turnContext.Activity.Text;
-            await turnContext.SendActivityAsync(test(responseMessage));
+            int choose = int.Parse(turnContext.Activity.Text);
+            if (choose == 1)
+            {
+                await turnContext.SendActivityAsync("¬ведите x1");
+                ushort x1 = ushort.Parse(turnContext.Activity.Text);
+                string result = kolvo(x1) + "";
+                await turnContext.SendActivityAsync("–езультат задачи є1{0}", result);
+
+            }
+
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
